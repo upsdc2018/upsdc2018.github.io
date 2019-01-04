b9e4a6adbd5372d9844a5ef18cf09b363006e8c1
mimin <optimusupperman@gmail.com>
2019-01-05 05:11:23 +0800

    test

--- js/test.js
+++ js/test.js
@@ -6,9 +6,32 @@ let context = canvas.getContext("2d");
 let base = 0;
 
 if (window.innerWidth > window.innerHeight) {
-    canvas.width = window.innerHeight;
-    canvas.height = window.innerHeight;
+    base = window.innerHeight;
+    canvas.width = base;
+    canvas.height = base;
 } else {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerWidth;
+    base = window.innerWidth;
+    canvas.width = base;
+    canvas.height = base;
+}
+
+canvas.style.margin = String(Math.floor((window.innerHeight - canvas.height) / 2)) + "px 0 0 0";
+
+// start
+
+canvas.addEventListener("touchstart", handleStart, false);
+canvas.addEventListener("touchend", handleEnd, false);
+// canvas.addEventListener("touchcancel", handleCancel, false);
+// canvas.addEventListener("touchmove", handleMove, false);
+
+function handleStart(event) {
+    event.preventDefault();
+    context.fillStyle = "#FF0000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function handleEnd(event) {
+    event.preventDefault();
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
